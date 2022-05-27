180dd8dc1e7acfd781103a85bce7b7078295e0c1
Timur Badretdinov <destinerx@gmail.com>
2022-05-27 17:48:09 +0400

    MetaInput styling

--- src/components/MetaInput.vue
+++ src/components/MetaInput.vue
@@ -34,5 +34,12 @@ function handleUpdate(value: string): void {
 
 <style scoped>
 .input {
+  --color-label: black;
+  --color-input: black;
+  --background-input: #eee;
+  --background-input-focus: #d9d9d9;
+  --border-input: 1px solid #b5b5b5;
+  --border-input-focus: 1px solid #b5b5b5;
+  --border-radius-input: 0;
 }
 </style>
