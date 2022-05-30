dae52153df941f38c4a2a1984f39379cd452c63f
Timur Badretdinov <destinerx@gmail.com>
2022-05-30 15:20:36 +0400

    Lint

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -30,30 +30,37 @@
   font-size: 18px;
   line-height: 1.6;
 }
+
 .content {
   max-width: 800px;
   margin-top: 24vh;
 }
+
 @media (min-width: 768px) {
   .content {
     min-width: 600px;
   }
 }
+
 a {
   color: #333;
 }
+
 .cta {
   margin-top: 32px;
 }
+
 /* Reset */
 *,
 *::before,
 *::after {
   box-sizing: border-box;
 }
+
 html {
   height: 100%;
 }
+
 img,
 picture,
 video,
@@ -62,12 +69,14 @@ svg {
   display: block;
   max-width: 100%;
 }
+
 input,
 button,
 textarea,
 select {
   font: inherit;
 }
+
 p,
 h1,
 h2,
