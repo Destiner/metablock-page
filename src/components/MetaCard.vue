2795670f81d894118af3b15153fa193ccaf2216f
Timur Badretdinov <destinerx@gmail.com>
2022-06-17 20:14:30 +0400

    Add MetaCard component

---
+++ src/components/MetaCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 16px;
+  border: 1px solid #a6a6a6;
+  background: #f2f2f2;
+  box-shadow: 4px 4px 4px #00000040;
+}
+</style>
